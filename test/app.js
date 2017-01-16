e80ba292ac23993cee89c69797c654fa211d1418
prathameshtajane <ptajane@gmail.com>
2017-01-15 23:08:15 -0500

    modified:   test/app.js

--- test/app.js
+++ test/app.js
@@ -5,6 +5,8 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
+ 
+ }
 
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
