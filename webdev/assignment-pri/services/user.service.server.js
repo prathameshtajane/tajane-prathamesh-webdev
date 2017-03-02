22cc0809eecba1144299d59c8f116f926fa9bf16
prathameshtajane <ptajane@gmail.com>
2017-03-01 22:49:30 -0500

    removed all console.log() from server.service and client.service

--- webdev/assignment-pri/services/user.service.server.js
+++ webdev/assignment-pri/services/user.service.server.js
@@ -38,9 +38,7 @@ module.exports=function (app) {
 
 
     function createUser(req,res){
-        console.log("reached createUser");
         var newUserInfo=req.body;
-        console.log(newUserInfo);
         var tempuser={};
         tempuser.username=newUserInfo.username;
         tempuser.password=newUserInfo.password1;
@@ -55,7 +53,6 @@ module.exports=function (app) {
         var password = req.query.password;
 
         if(username && password){
-        console.log("Both username and password present so calling findUserByCredentails");
         findUserByCredentails(req,res);
         }
         else{
@@ -64,7 +61,6 @@ module.exports=function (app) {
     }
 
     function findUserByUsername(req,res) {
-        console.log("Only username present so calling findUserByUsername");
         var user = users.find(function (u) {
            return u.username === req.query.username;
         });
@@ -77,7 +73,6 @@ module.exports=function (app) {
     }
 
     function updateUser(req,res){
-        console.log("Reached updateUser");
         var userid=req.params.userId;
         var newUserInfo=req.body;
         for(var usr in users){
@@ -87,7 +82,6 @@ module.exports=function (app) {
                 users[selectedIndex].email=newUserInfo.email;
                 users[selectedIndex].firstName=newUserInfo.Firstname;
                 users[selectedIndex].lastName=newUserInfo.Lastname;
-                /*return angular.copy(users[usr]);*/
                 res.json(users[usr]);
                 return;
             }
@@ -96,7 +90,6 @@ module.exports=function (app) {
 
     function findUserById(req,res){
         var userid=req.params.userId;
-        /*console.log(userid);*/
         var user = users.find(function (user){
             return user._id == userid;
         });
@@ -107,9 +100,6 @@ module.exports=function (app) {
     function findUserByCredentails(req,res){
         var queryUserName=req.query.username;
         var queryUserPassword=req.query.password;
-        /*console.log(queryUserName);
-        console.log(queryUserPassword);*/
-
         var user=users.find(function(user){
            return user.password==queryUserPassword && user.username==queryUserName
         });
@@ -120,10 +110,6 @@ module.exports=function (app) {
     function deleteUser(req,res) {
         console.log("Reached deleteUser");
         var userid = req.params.userId;
-        /*var user=users.find(function (u) {
-            console.log(u);
-            return parseInt(u._id)===parseInt(userid);
-        });*/
         for(var usr in users){
             if (parseInt(users[usr]._id) === parseInt(userid)){
                 var index_tobe_deleted=users.indexOf(users[usr]);
